fe10a06564f6f24d223675de4809bf075606ba7b
Timothy Gaweco <tim.gaweco@gmail.com>
2013-12-14 17:37:03 -0500

    mobile nav bar finished

---
+++ navigation.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	$("#mobile-icon button").on('click', function() {
+		$("#mobile-menu").slideToggle("medium");
+	});
+
+	// If window size is below 850px and nav bar is down, move it up 
+
+});
