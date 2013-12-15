34a34a78bc08d5891f2d0ff9d1e069e939ec18dd
Timothy Gaweco <tim.gaweco@gmail.com>
2013-12-14 23:33:45 -0500

    fixed bug on mobile nav

--- navigation.js
+++ navigation.js
@@ -4,8 +4,12 @@ $(document).ready(function(){
 	});
 
 	// If window size is below 850px and nav bar is down, move it up 
-	
+	$(window).resize(function() {
+		var width = $(window).width();
+		var mobileMenu = $('#mobile-menu').css('display');
 
-
-	
+		if (width > 850 && (mobileMenu != 'none')) {
+			$("#mobile-menu").slideUp("fast");
+		}
+	});
 });
